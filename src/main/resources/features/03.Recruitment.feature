5a94fdf157f52896faff0c5cb6d863e0d65161b4
Rizki Maulana Azis <rizkimazis12@gmail.com>
2022-10-28 16:24:54 +0700

    First Commit

--- src/main/resources/features/03.Recruitment.feature
+++ src/main/resources/features/03.Recruitment.feature
@@ -1,5 +1,7 @@
 Feature: Recruitment User
 
   Scenario: Recruitment user function
-    When User enter username password valid
+    When User click menu Recruitment
+    And User on menu Recruitment
+    And User enter name by hiring manager
     Then User click button search
--- src/test/java/com/juaracoding/cucumber/step_definitions/TestRecruitment.java
+++ src/test/java/com/juaracoding/cucumber/step_definitions/TestRecruitment.java
@@ -1,81 +1,51 @@
 package com.juaracoding.cucumber.step_definitions;
 
-import com.juaracoding.cucumber.drivers.DriverSingleton;
-import com.juaracoding.cucumber.pages.LoginPage;
 import com.juaracoding.cucumber.pages.RecruitmentPage;
-import com.juaracoding.cucumber.utils.Constants;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
 
 public class TestRecruitment {
-    public static WebDriver driver;
 
+    WebDriver driver;
     private static ExtentTest extentTest;
-    private LoginPage login;
-    private RecruitmentPage recruitment;
+    private final RecruitmentPage recruitmentPage = new RecruitmentPage();
 
     public TestRecruitment() {
         driver = com.juaracoding.cucumber.step_definitons.Hooks.driver;
         extentTest = com.juaracoding.cucumber.step_definitons.Hooks.extentTest;
     }
 
-    @BeforeClass
-    public void setUp() {
-        DriverSingleton.getInstance(Constants.CHROME);
-        driver = DriverSingleton.getDriver();
-        driver.get(Constants.URL);
+    @When("User click menu Recruitment")
+    public void user_click_menu_recruitment() {
+//		Hooks.delay(1);
+        recruitmentPage.toString();
+        extentTest.log(LogStatus.PASS, "User click menu Recruitment");
     }
 
-    @BeforeMethod
-    public void pageObject() {
-        login = new LoginPage();
-        recruitment = new RecruitmentPage();
+    @And("User on menu Recruitment")
+    public void user_on_menu_recruitment() {
+//		Hooks.delay(1);
+        recruitmentPage.toString();
+        extentTest.log(LogStatus.PASS, "User on menu Recruitment");
     }
 
-    @AfterClass
-    public void closeBrowser() {
-        delay(3);
-        DriverSingleton.closeObjectInstance();
-    }
-
-    @When("User enter username password valid")
-    public void user_enter_username_password_valid() {
-        delay(Constants.DETIK);
-        // PreCondition
-        login.login("Admin", "admin123");
-        delay(Constants.DETIK);
-        // Step
-        //noinspection ResultOfMethodCallIgnored
-        recruitment.toString();
-        Assert.assertEquals(recruitment.toString(), "Recruitment");
-        System.out.println("Test Case Recruitment Page");
+    @And("User enter name by hiring manager")
+    public void User_enter_name_by_hiring_manager() {
+//		Hooks.delay(1);
+        recruitmentPage.toString();
+        extentTest.log(LogStatus.PASS, "User enter name by hiring manager");
     }
 
     @Then("User click button search")
-    public void User_click_button_search() {
-        delay(Constants.DETIK);
-        // Step
-        //noinspection ResultOfMethodCallIgnored
-        recruitment.toString();
-        Assert.assertTrue(recruitment.toString().contains("Linda"));
-        System.out.println("Test Case Search By Hiring Manager");
+    public void user_click_button_search() {
+        Assert.assertEquals(recruitmentPage.toString(), "Record Found");
+        extentTest.log(LogStatus.PASS, "User click button search");
     }
 
-    static void delay(int detik) {
-        try {
-            Thread.sleep(5000L * detik);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
 }
-
-
